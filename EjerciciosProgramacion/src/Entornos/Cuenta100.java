d3e58fab35fb4147ccf30d7df08c17a06fc0f4fe
a023326273h <a023326273h@S09-PC15>
2021-11-22 19:54:25 +0100

    Ejercicios entornos

---
+++ EjerciciosProgramacion/src/Entornos/Cuenta100.java
@@ -0,0 +1,34 @@
+/*
+ * Proyecto UD3_ejercicios - Archivo Cuenta100.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package Entornos;
+
+/**
+ *
+ * @author Kevin Blanco Bañuls <blancodaw22@gmail.com>
+ * @version 1.0
+ * @date 3 nov. 2021 16:37:51
+ */
+public class Cuenta100 {
+
+    
+    
+    public static void main(String[] args) {
+        int inicio = 0;
+        int fin = 100;
+        
+        while(inicio < fin) {
+            inicio++;
+            System.out.println(inicio);
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
---
+++ EjerciciosProgramacion/src/Entornos/PidoNumero.java
@@ -0,0 +1,38 @@
+/*
+ * Proyecto UD3_ejercicios - Archivo PidoNumero.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package Entornos;
+
+/**
+ *
+ * @author Kevin Blanco Bañuls <blancodaw22@gmail.com>
+ * @version 1.0
+ * @date 3 nov. 2021 16:52:59
+ */
+import java.util.Scanner;
+
+public class PidoNumero {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numero = 1;
+        int fin = 10;
+        int respuesta;
+        System.out.println("Dime un numero del 1 al 10");
+        respuesta = in.nextInt();
+        if(respuesta <= fin ){
+            System.out.println(respuesta);
+            
+        }
+        else{
+            
+        }
+        
+        }
+            
+        
+    }
+
