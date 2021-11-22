0a2d9fb2a331da88360cd3b43f61becc3bebd554
a023326273h <a023326273h@S09-PC15>
2021-11-22 19:46:16 +0100

    Repositorio para ejercicios programación

---
+++ EjerciciosProgramacion/src/ejerciciosprogramacion/EjerciciosProgramacion.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto EjerciciosProgramacion - Archivo EjerciciosProgramacion.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package ejerciciosprogramacion;
+
+/**
+ *
+ * @author Kevin Blanco Bañuls <blancodaw22@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:45:00
+ */
+public class EjerciciosProgramacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
